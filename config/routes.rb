07a7dbff8eb064121ffb0d090e05981e582be0dc
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-09-22 16:38:35 +0000

    [Finish] bok15

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  resources :users, only: [:show, :edit]
+  
+  resources :books, only: [:new, :index, :show]
+  
+  devise_for :users
+  
+  root to: "homes#top"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
