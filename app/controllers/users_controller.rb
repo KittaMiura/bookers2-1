3198f7c9e60297d43c06cbb5a7f1f66f6a865c30
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-09-23 03:16:16 +0000

    [Finish] bok45

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,34 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:page])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def index
+    @users = User.page(params[:page]) 
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+  
 end
