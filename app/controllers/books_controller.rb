99f20492f35aa71a5cb79f2839783f7238669cb3
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-09-23 14:01:54 +0000

    [Start] boostrap2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,39 @@
 class BooksController < ApplicationController
 
   def new
+    @book = Book.new
   end
 
   def index
     @book = Book.new
-    @books = Book.page(params[:page]) 
+    @books = Book.all
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to @book
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title,  :opinion)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,16 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books.page(params[:page])
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def index
-    @users = User.page(params[:page]) 
   end
-  
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
-  end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
@@ -30,5 +22,5 @@ private
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
 end
